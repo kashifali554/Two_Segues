95cc698934a9bbb7cd7a142ac13cf4148c3f5345
Kashif Ali <kashifali874@gmail.com>
2018-10-29 17:13:09 -0700

    Simple project files

---
+++ Two_Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two_Segues
+//
+//  Created by Ali on 10/29/18.
+//  Copyright © 2018 Kashif Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
