4f2befe39d6d7b4e513eeebe43edecd0686ba9c7
Kashif Ali <kashifali874@gmail.com>
2018-10-29 17:52:04 -0700

    UI, second view and Segue created

---
+++ Two_Segues/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  Two_Segues
+//
+//  Created by Ali on 10/29/18.
+//  Copyright © 2018 Kashif Ali. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func sendDataBack(_ sender: Any) {
+        
+    }
+}
--- Two_Segues/ViewController.swift
+++ Two_Segues/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+    }
+    
 }
 
